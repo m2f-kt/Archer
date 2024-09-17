6b9fb2371ed8b978639c1e0220176b31ab068e54
Marc Moreno Ferrer <ignis.fatue@gmail.com>
2024-09-17 13:06:35 +0200

    feat(core): Add kleisli arrow to repository

--- archer-core/src/commonMain/kotlin/com/m2f/archer/mapper/KleisliArrow.kt
+++ archer-core/src/commonMain/kotlin/com/m2f/archer/mapper/KleisliArrow.kt
@@ -2,6 +2,8 @@ package com.m2f.archer.mapper
 
 import com.m2f.archer.crud.ArcherRaise
 import com.m2f.archer.datasource.DataSource
+import com.m2f.archer.repository.Repository
+import com.m2f.archer.repository.toRepository
 
 fun <Q, A, B> DataSource<Q, A>.andThen(f: suspend ArcherRaise.(A) -> B): DataSource<Q, B> =
     DataSource<Q, B> { query ->
@@ -12,3 +14,13 @@ fun <Q, A, B> DataSource<Q, A>.andThen(ds: DataSource<A, B>): DataSource<Q, B> =
     DataSource<Q, B> { query ->
         ds.execute(execute(query))
     }
+
+fun <Q, A, B> Repository<Q, A>.andThen(f: suspend ArcherRaise.(A) -> B): Repository<Q, B> =
+    DataSource<Q, B> { query ->
+        f(execute(query))
+    }.toRepository()
+
+fun <Q, A, B> Repository<Q, A>.andThen(ds: Repository<A, B>): Repository<Q, B> =
+    DataSource<Q, B> { query ->
+        ds.execute(execute(query))
+    }.toRepository()
--- archer-core/src/commonTest/kotlin/com/m2f/archer/datasource/KleisliArrowTest.kt
+++ archer-core/src/commonTest/kotlin/com/m2f/archer/datasource/KleisliArrowTest.kt
@@ -2,13 +2,14 @@ package com.m2f.archer.datasource
 
 import com.m2f.archer.crud.Ice
 import com.m2f.archer.mapper.andThen
+import com.m2f.archer.repository.toRepository
 import com.m2f.archer.utils.archerTest
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 class KleisliArrowTest : FunSpec({
 
-    archerTest("thunk kleisli arrow") {
+    archerTest("Datasource thunk kleisli arrow") {
         var effect = 0
         val a = DataSource<Int, String> { effect.toString().also { effect++ } }
         val b = DataSource<Unit, Int> { 0 }
@@ -23,6 +24,28 @@ class KleisliArrowTest : FunSpec({
     archerTest("DataSource kleisli arrow") {
         val a = DataSource<Int, String> { 3.toString() }
         val b = DataSource<String, Int> { it.toInt() }
+        val c = a.andThen(b)
+
+        ice { c.execute(0) } shouldBe Ice.Content(3)
+        ice { a.execute(0) } shouldBe Ice.Content("3")
+        ice { b.execute("0") } shouldBe Ice.Content(0)
+    }
+
+    archerTest("Repository thunk kleisli arrow") {
+        var effect = 0
+        val a = DataSource<Int, String> { effect.toString().also { effect++ } }.toRepository()
+        val b = DataSource<Unit, Int> { 0 }.toRepository()
+        val c = a.andThen { string -> b.execute() }
+
+        ice { c.execute(0) } shouldBe Ice.Content(0)
+        ice { a.execute(0) } shouldBe Ice.Content("1")
+        ice { b.execute() } shouldBe Ice.Content(0)
+        effect shouldBe 2
+    }
+
+    archerTest("Repository kleisli arrow") {
+        val a = DataSource<Int, String> { 3.toString() }.toRepository()
+        val b = DataSource<String, Int> { it.toInt() }.toRepository()
         val c = a.andThen(b)
 
         ice { c.execute(0) } shouldBe Ice.Content(3)
