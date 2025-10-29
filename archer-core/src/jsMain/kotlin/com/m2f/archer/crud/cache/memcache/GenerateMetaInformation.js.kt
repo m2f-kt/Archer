60ecf2e01617afae395e089b68c51cb74ccd6b7d
Marco Gomiero <marco.gomiero.93@gmail.com>
2025-10-29 15:58:26 +0100

    refactor(memcache): unify  implementation across all platforms and bump version to 1.0.7

--- archer-core/src/jsMain/kotlin/com/m2f/archer/crud/cache/memcache/GenerateMetaInformation.js.kt
+++
@@ -1,9 +0,0 @@
-package com.m2f.archer.crud.cache.memcache
-
-actual inline fun <reified A> getMetaInformation(key: String): CacheMetaInformation =
-    CacheMetaInformation(
-        key = key,
-        classIdentifier = A::class.let {
-            it.simpleName ?: "Unknown"
-        }
-    )
